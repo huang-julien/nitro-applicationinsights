63d5ddda2216b0cccd915580e0b1f3fb1e4ee4a9
julien huang <julien.huang@outlook.fr>
2023-09-21 22:03:02 +0200

    feat: provide basic middleware, plugin, setup

---
+++ src/plugin.ts
@@ -0,0 +1,24 @@
+import { defineNitroPlugin } from "nitropack/dist/runtime/plugin";
+import { setup } from "./setup"
+
+export default defineNitroPlugin(nitro => {
+    setup()
+
+    nitro.hooks.hook('render:response', (response, { event }) => {
+        event.__appInsights.client.trackRequest({
+            name: `${event.method}: ${event.path}`,
+            url: event.path,
+            resultCode: response.statusCode ?? 0,
+            duration: Date.now() - event.__appInsights.startTime,
+            success: response.statusCode ? response.statusCode < 400 : false,
+            properties: event.__appInsights.properties,
+            contextObjects: {
+                ...event.__appInsights.client.context.tags,
+                // needed ? 
+                // [event.__appInsights.client.context.keys.operationId]: event.__appInsights.trace.traceId,
+                [event.__appInsights.client.context.keys.operationParentId]: event.__appInsights.trace.parentId,
+            },
+            id: event.__appInsights.trace.traceId
+        })
+    })
+})
---
+++ src/types.d.ts
@@ -0,0 +1,21 @@
+import type { TelemetryClient } from "applicationinsights";
+import type Traceparent from "applicationinsights/out/Library/Traceparent";
+import type { H3Event } from "h3"
+
+declare module 'h3' {
+    interface H3Event {
+        __appInsights: {
+            startTime: number
+            client: TelemetryClient
+            trace: Traceparent
+            initialTrace: string
+            properties: Record<string, string>
+        }
+    }
+}
+
+declare module 'nitropack/dist/runtime/types.d.ts' {
+    interface NitroRuntimeHooks {
+        'applicationinsights:context:tags': (client: TelemetryClient, tags: Record<string, string>, context: { event: H3Event }) => void
+    }
+}
