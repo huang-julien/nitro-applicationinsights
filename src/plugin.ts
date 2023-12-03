99719595eac3f1f6a2921da99fea6b671cee79d9
julien huang <julien.huang@outlook.fr>
2023-12-03 14:23:20 +0100

    fix: add initial trace as operation id

--- src/plugin.ts
+++ src/plugin.ts
@@ -96,9 +96,10 @@ export default defineNitroPlugin(async (nitro) => {
           ...event.$appInsights.client.context.tags,
           [event.$appInsights.client.context.keys.operationParentId]:
             event.$appInsights.trace.parentId,
-          [event.$appInsights.client.context.keys.operationName]: name
+          [event.$appInsights.client.context.keys.operationName]: name,
+          [event.$appInsights.client.context.keys.operationId]: event.$appInsights.initialTrace
         },
-        id: event.$appInsights.trace.traceId
+        id: event.$appInsights.trace.traceId,
       }
 
       await nitro.hooks.callHook('applicationinsights:trackRequest:before', event, trackInfo)
