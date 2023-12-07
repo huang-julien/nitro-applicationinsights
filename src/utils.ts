0b18ebf1f80220d8388b1475a23a18c070b35568
julien huang <julien.huang@outlook.fr>
2023-12-07 22:26:21 +0100

    chore: lint

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,15 @@
-import type { H3Event } from "h3";
-import { Nitro, NitroFetchOptions } from "nitropack";
-import type { FetchOptions } from "ofetch";
+import type { H3Event } from 'h3'
+import { NitroFetchOptions } from 'nitropack'
 
 /**
  * return an object with the traceparent
  * @param {H3Event} event - The event object from h3.
  * @param {Record<string, string>} headers - optional headers to add to the object
  */
-export function getTraceparentHeader(event: H3Event, headers: HeadersInit = {}): HeadersInit {
-    return Object.assign(headers, {
-        traceparent: event.$appInsights.trace.toString()
-    })
+export function getTraceparentHeader (event: H3Event, headers: HeadersInit = {}): HeadersInit {
+  return Object.assign(headers, {
+    traceparent: event.$appInsights.trace.toString()
+  })
 }
 
 /**
@@ -18,59 +17,58 @@ export function getTraceparentHeader(event: H3Event, headers: HeadersInit = {}):
  * @param {H3Event} event - The event object from h3.
  * @returns {TelemetryClient} The telemetry client from `applicationinsights`.
  */
-export function getEventTelemetryClient(event: H3Event) {
-    return event.$appInsights.client
+export function getEventTelemetryClient (event: H3Event) {
+  return event.$appInsights.client
 }
-
 
 /**
  * Returns the FetchOptions object with interceptors for tracking dependencies and exceptions.
- * Note that if you need to set this dependency as children of an event, the event must 
+ * Note that if you need to set this dependency as children of an event, the event must
  * @param event - The H3Event object.
  * @returns The FetchOptions object.
  */
-export function get$FetchInterceptors(event?: H3Event): NitroFetchOptions<any, any> {
-    let startTime: number | undefined
-    const contextObjects: Record<string, string> = {}
-    if (event) {
-        contextObjects[event.$appInsights.client.context.keys.operationId] = event.$appInsights.trace.toString()
-        contextObjects[event.$appInsights.client.context.keys.operationParentId] = event.$appInsights.trace.traceId
+export function get$FetchInterceptors (event?: H3Event): NitroFetchOptions<any, any> {
+  let startTime: number | undefined
+  const contextObjects: Record<string, string> = {}
+  if (event) {
+    contextObjects[event.$appInsights.client.context.keys.operationId] = event.$appInsights.trace.toString()
+    contextObjects[event.$appInsights.client.context.keys.operationParentId] = event.$appInsights.trace.traceId
+  }
+  return {
+    onRequest (context) {
+      if (event) {
+        context.options.headers = getTraceparentHeader(event, context.options.headers ?? {})
+      }
+      startTime = Date.now()
+    },
+    onRequestError (context) {
+      event?.$appInsights.client.trackException(
+        {
+          contextObjects,
+          exception: context.error
+        })
+    },
+    onResponse (context) {
+      event?.$appInsights.client.trackDependency({
+        dependencyTypeName: 'HTTP',
+        duration: startTime ? (Date.now() - startTime) : 0,
+        name: context.response.url,
+        data: '',
+        resultCode: context.response.status,
+        success: true,
+        contextObjects
+      })
+    },
+    onResponseError (context) {
+      event?.$appInsights.client.trackDependency({
+        dependencyTypeName: 'HTTP',
+        duration: startTime ? (Date.now() - startTime) : 0,
+        name: context.response.url,
+        data: '',
+        resultCode: context.response.status,
+        success: false,
+        contextObjects
+      })
     }
-    return {
-        onRequest(context) {
-            if (event) {
-                context.options.headers = getTraceparentHeader(event, context.options.headers ?? {})
-            }
-            startTime = Date.now()
-        },
-        onRequestError(context) {
-            event?.$appInsights.client.trackException(
-                {
-                    contextObjects,
-                    exception: context.error,
-                })
-        },
-        onResponse(context) {
-            event?.$appInsights.client.trackDependency({
-                dependencyTypeName: 'HTTP',
-                duration: startTime ? (Date.now() - startTime) : 0,
-                name: context.response.url,
-                data: '',
-                resultCode: context.response.status,
-                success: true,
-                contextObjects
-            })
-        },
-        onResponseError(context) {
-            event?.$appInsights.client.trackDependency({
-                dependencyTypeName: 'HTTP',
-                duration: startTime ? (Date.now() - startTime) : 0,
-                name: context.response.url,
-                data: '',
-                resultCode: context.response.status,
-                success: false,
-                contextObjects
-            })
-        },
-    }
+  }
 }
