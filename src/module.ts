4a35acc594cf020d7dc730c3dc5fa801d446d4f0
Julien Huang <julien.h.dev@gmail.com>
2023-12-23 21:43:52 +0100

    chore: lint

--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,10 @@
-import type { NitroModule } from "nitropack"
-import { resolve, dirname } from "pathe"
-import { fileURLToPath } from "url"
+import { fileURLToPath } from 'node:url'
+import type { NitroModule } from 'nitropack'
+import { resolve, dirname } from 'pathe'
 
 export default <NitroModule>{
-    name: 'nitro-applicationinsights',
-    setup(nitro) {
-        nitro.options.plugins.push(resolve(dirname(fileURLToPath(import.meta.url)), './runtime/plugin'))
-    }
+  name: 'nitro-applicationinsights',
+  setup (nitro) {
+    nitro.options.plugins.push(resolve(dirname(fileURLToPath(import.meta.url)), './runtime/plugin'))
+  }
 }
