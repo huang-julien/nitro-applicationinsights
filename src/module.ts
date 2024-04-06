0374779aea3cfa7df8335b58463d95d4e5d6f7a6
julien huang <julien.huang@outlook.fr>
2024-04-06 21:04:20 +0200

    fix: enable legacy externals

--- src/module.ts
+++ src/module.ts
@@ -4,7 +4,14 @@ import { resolve, dirname } from 'pathe'
 
 export default <NitroModule>{
   name: 'nitro-applicationinsights',
-  setup (nitro) {
+  setup(nitro) {
+    // this fix the detection of applicationinsights as esm while waiting for mlly 2.0
+    if (!nitro.options.experimental?.legacyExternals) {
+      if (!nitro.options.experimental) {
+        nitro.options.experimental = {}
+      }
+      nitro.options.experimental.legacyExternals = true
+    }
     nitro.options.plugins.push(resolve(dirname(fileURLToPath(import.meta.url)), './runtime/plugin'))
   }
 }
