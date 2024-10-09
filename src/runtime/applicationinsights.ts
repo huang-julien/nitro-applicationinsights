607dbd4f510729a37ca11837b875472ef251e595
Julien Huang <julien.h.dev@gmail.com>
2024-10-09 20:48:00 +0200

    refactor: use export statement (#113)

--- src/runtime/applicationinsights.ts
+++ src/runtime/applicationinsights.ts
@@ -1,6 +1,3 @@
 // this file re-export the applicationinsights module. plugin.ts is inlined so we need to avoid having applicationinsights being inlined as well
-
-import * as applicationinsights from 'applicationinsights';
+export { default } from 'applicationinsights';
 export * from "applicationinsights"
-// @ts-ignore wtf applicationinsights ????
-export default applicationinsights.default
