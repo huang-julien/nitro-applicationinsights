f07890f148d5c45c6133b24d4332bdaf85b584e6
Julien Huang <julien.h.dev@gmail.com>
2025-01-11 00:26:49 +0100

    test: cleanup

--- test/fixtures/basic/routes/with-dependency.ts
+++ test/fixtures/basic/routes/with-dependency.ts
@@ -1,15 +1,11 @@
  import { context, propagation } from "@opentelemetry/api"
 
 export default defineTracedEventHandler(async () => {
-    const { trace } = await $fetch<{ trace: string }>('/some-dep',
- 
-) 
-    const { trace: trace2} = await globalThis.fetch('http://localhost:3000/some-dep',).then((res) => res.json()    )
+    const { trace } = await $fetch<{ trace: string }>('/some-dep') 
     const carrier: any = {}
     propagation.inject(context.active(), carrier)
     return {
         trace: carrier.traceparent,
         dependencyTrace: trace,
-        trace2
     }
 })
