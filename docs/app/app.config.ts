e0e54a28d9286a6178b7b4b8d5fa6a8a7ff4419a
Julien Huang <julien.h.dev@gmail.com>
2025-03-26 22:09:02 +0100

    docs: move to nuxt ui pro template

---
+++ docs/app/app.config.ts
@@ -0,0 +1,76 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'green',
+    gray: 'slate',
+    footer: {
+      bottom: {
+        left: 'text-sm text-gray-500 dark:text-gray-400',
+        wrapper: 'border-t border-gray-200 dark:border-gray-800'
+      }
+    },   
+    variables: {
+      light: {
+        background: '255 255 255',
+        foreground: 'var(--color-gray-700)'
+      },
+      dark: {
+        background: 'var(--color-gray-900)',
+        foreground: 'var(--color-gray-200)'
+      },
+      header: {
+        height: '4rem'
+      }
+    }
+  },
+  seo: {
+    siteName: 'nitro-applicationinsights'
+  },
+  header: {
+    logo: {
+      alt: '',
+      light: '',
+      dark: ''
+    },
+    search: true,
+    colorMode: true,
+    links: [{
+      icon: 'i-simple-icons-github',
+      to: 'https://github.com/huang-julien/nitro-applicationinsights',
+      target: '_blank',
+      'aria-label': 'nitro-applicationinsights github'
+    }]
+  },
+  footer: {
+    credits: 'Copyright © 2024',
+    colorMode: false,
+    links: [{
+      text: 'i-simple-icons-github',
+      to: 'https://github.com/huang-julien/nitro-applicationinsights',
+      target: '_blank',
+      'aria-label': 'Nitro applicationinsights github'
+    }]
+  },
+  toc: {
+    title: 'Table of Contents',
+    bottom: {
+      title: 'Community',
+      edit: 'https://github.com/nuxt-ui-pro/docs/edit/main/content',
+      links: [{
+        icon: 'i-heroicons-star',
+        label: 'Star on GitHub',
+        to: 'https://github.com/nuxt/ui',
+        target: '_blank'
+      }, {
+        icon: 'i-heroicons-book-open',
+        label: 'Nuxt UI Pro docs',
+        to: 'https://ui.nuxt.com/pro/guide',
+        target: '_blank'
+      }, {
+        icon: 'i-simple-icons-nuxtdotjs',
+        label: 'Purchase a license',
+        to: 'https://ui.nuxt.com/pro/purchase',
+        target: '_blank'
+      }]
+    }
+  }
+})
