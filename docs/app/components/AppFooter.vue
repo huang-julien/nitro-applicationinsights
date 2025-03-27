bbd9dbfd14a1ba38a375c2bdafbe42c730ce1cd3
Julien Huang <julien.h.dev@gmail.com>
2025-03-27 22:31:47 +0100

    docs: move to nuxt ui pro template (#163)

---
+++ docs/app/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { footer } = useAppConfig()
+</script>
+
+<template>
+  <UFooter>
+    <template #left>
+      {{ footer.credits }}
+    </template>
+
+    <template #right>
+      <UColorModeButton v-if="footer?.colorMode" />
+
+      Created and maintained by <ULink
+        href="https://github.com/huang-julien"
+        target="_blank"
+      >
+        Julien Huang
+      </ULink>
+    </template>
+  </UFooter>
+</template>
