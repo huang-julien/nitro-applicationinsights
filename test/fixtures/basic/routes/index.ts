94b520cb56fd2e10b78a71276e3eb73f904b9188
Julien Huang <julien.h.dev@gmail.com>
2025-08-13 21:18:35 +0200

    test: fix breaking change ...

--- test/fixtures/basic/routes/index.ts
+++ test/fixtures/basic/routes/index.ts
@@ -6,6 +6,6 @@ export default defineTracedEventHandler((event) => {
     return {
         trace: carrier.traceparent,
 
-         parentSpanId: event.otel.span.parentSpanId,
+         parentSpanId: event.otel.span.parentSpanContext.spanId,
     }
 })
